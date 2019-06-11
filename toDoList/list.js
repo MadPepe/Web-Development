6516a32d82b389c8d6c2da5417299c9f4d187650
Mateusz Stolowski <mateuszstolowski95@gmail.com>
2019-06-11 12:46:38 +0200

    Added delete argument to todos list

--- toDoList/list.js
+++ toDoList/list.js
@@ -9,7 +9,26 @@ while(choice !== "quit"){
         list.push(element);
     }
     else if(choice === "list"){
-        console.log(list);
+        console.log("*******************");
+
+        list.forEach(function(element, index){
+            console.log(index+": " + element);
+        });
+
+        console.log("*******************");
+    }
+    else if(choice === "delete"){
+        element = prompt("Which element do you want to delete?");
+        if(list.indexOf(element) !== -1){
+            list = list.filter(function(it){
+                return it !== element;
+            });
+            console.log("Element " + element + " deleted");
+        }
+        else {
+            console.log("There is no element " + element + " in Todo's list!");
+        }
+        
     }
     else if(choice != 'quit'){
        alert("Whoops! It appears that you entered not supported action.\n Please try again");
